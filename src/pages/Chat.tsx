df468559df9e7760abefdaeac336a97bd3356305
TriskyXD <vikulka01@centrum.cz>
2024-02-02 13:31:57 +0100

    basic design - chat window - update

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,24 +1,26 @@
 import { Button, Container, Form, Stack, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom"; // Import the Link component
+import { Link } from "react-router-dom";
 
 const Chat = () => {
     return (
         <Container>
             <Stack direction={"horizontal"} className={"align-items-start"} gap={4}>
                 {/* Side contact list */}
-                <div style={{ border: "1px solid #ccc", padding: "10px", borderRadius: "5px", width: "350px", height:"550px", overflowY: "auto" }}>
+                <div className={"d-flex flex-column"} style={{ border: "1px solid #ccc", padding: "10px", borderRadius: "5px", width: "350px", height:"550px", overflowY: "auto" }}>
                     <Stack className={"flex-grow-1 message-box pe-3"} gap={3}>
-                        <div style={{ backgroundColor: "black", height: "80px" }} /> {/* White background to fill the space */}
-                        <div className="headind_srch" style={{ position: "fixed", backgroundColor: "white", width:"300px", paddingTop:"5px"}}>
-                            <Form>
-                                <Form.Group className="mb-3">
-                                    <Form.Control type="search" placeholder="Search" />
+
+
+                             <Form className="bg-white position-sticky top-0">
+                                <Form.Group className="text-muted d-flex justify-content-start align-items-center pe-3 pt-3 mt-2">
+                                    <Form.Control type="text" placeholder="Type message" />
+                                    <Button variant="primary" type="button">
+                                        Odeslat
+                                    </Button>
                                 </Form.Group>
                             </Form>
-                        </div>
 
                         {/* List of Chats */}
-                        <div className="chat_list" style={{paddingTop: "50px"}} >
+                        <div className="chat_list" >
                             <div className="chat_people">
                                 <Link to="/chat">
                                     <div className="chat_ib">
@@ -152,14 +154,33 @@ const Chat = () => {
                 </div>
 
                 {/* Main chat window */}
-                <div className="flex-grow-1 message-box d-flex flex-column justify-content-end" style={{ border: "1px solid #ccc", padding: "10px", borderRadius: "5px", overflowY: "auto", height:"550px"}}>
+                <div className="flex-grow-1 message-box d-flex flex-column " style={{ border: "1px solid #ccc", padding: "10px", borderRadius: "5px", overflowY: "auto", height: "550px" }}>
+
                     <Stack direction="vertical" gap={3} className="justify-content-end">
-                        <p className="text-start">Chat1</p>
-                        <p className="text-end">Chat2</p>
-                        <p className="text-start">Chat3</p>
+                        <div className="bubble bubble-left">Chat1</div>
+                        <div className="bubble bubble-right">Chat2</div>
+                        <div className="bubble bubble-left">Chat3 - pokus jestli resize ahoj ....................................
+                        .......................................................
+                        .......................</div>
+                        <div className="bubble bubble-right">Chat - pokus jestli resize ahoj ....................................
+                            .......................................................
+                            .......................</div>
+                        <div className="bubble bubble-left">Chat3 - pokus jestli resize ahoj ....................................
+                            .......................................................
+                            .......................</div>
+                        <div className="bubble bubble-right">Chat - pokus jestli resize ahoj ....................................
+                            .......................................................
+                            .......................</div>
+                        <div className="bubble bubble-left">Chat3 - pokus jestli resize ahoj ....................................
+                            .......................................................
+                            .......................</div>
+                        <div className="bubble bubble-right">Chat - pokus jestli resize ahoj ....................................
+                            .......................................................
+                            .......................</div>
+                        {/* Add more chat bubbles as needed */}
                     </Stack>
-                    <Form className="mt-auto">
-                        {/* Additional Search input inside the ChatBox */}
+
+                    <Form className="position-sticky bottom-0">
                         <Form.Group className="text-muted d-flex justify-content-start align-items-center pe-3 pt-3 mt-2">
                             <Form.Control type="text" placeholder="Type message" />
                             <Button variant="primary" type="button">
