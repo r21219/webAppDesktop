e53fc4f1f774b8fcf51ba486cd226d7db04f725a
r21219 <r21219@student.osu.cz>
2024-04-17 16:03:13 +0200

    Added wireframes and todos for the future development of the FE

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,13 @@ import { AuthProvider } from './context/AuthContext';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container} from "react-bootstrap";
-
-
+//Todo
+// 1. whenever a user logs in or registers they will be connected to ws and sent do the chat window
+// 2. the UI design will follow the design in the docs
+// 3. Once in the chat window you can create a conversation by clicking a button which will make window for creating a conversation appear
+// 3.1 in that window you can search for users and click on their name to add them (will be added to an internal list which will be send to API)
+// 3.2 either way will show a message on the status if it was or was not created if it was the conversation will be immediately added to other conversations
+// 3.3 When a user receives a message in a conversations that conversations div will have an extra icon to imply a message was received or just use TOAST for that
 function App() {
   return (
       <>
